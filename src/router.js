905cc2d26826f9e5917e7985b2fce0197cef6b0f
alexraduca01 <alex.raduca01@icloud.com>
2024-03-04 17:34:03 +0100

    added region routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from './views/AppHome.vue'
-import AppKanto from './components/AppKanto.vue'
+import AppKanto from './views/AppKanto.vue'
+import AppJohto from './views/AppJohto.vue'
+import AppHoenn from './views/AppHoenn.vue'
+import AppSinnoh from './views/AppSinnoh.vue'
+import AppUnova from './views/AppUnova.vue'
+import AppKalos from './views/AppKalos.vue'
+import AppAlola from './views/AppAlola.vue'
+import AppGalar from './views/AppGalar.vue'
+import AppPaldea from './views/AppPaldea.vue'
+import AppHisui from './views/AppHisui.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +23,51 @@ const router = createRouter({
       path: '/kanto',
       name: 'kanto',
       component: AppKanto
+    },
+    {
+      path: '/johto',
+      name: 'johto',
+      component: AppJohto
+    },
+    {
+      path: '/hoenn',
+      name: 'hoenn',
+      component: AppHoenn
+    },
+    {
+      path: '/sinnoh',
+      name: 'sinnoh',
+      component: AppSinnoh
+    },
+    {
+      path: '/unova',
+      name: 'unova',
+      component: AppUnova
+    },
+    {
+      path: '/kalos',
+      name: 'kalos',
+      component: AppKalos
+    },
+    {
+      path: '/alola',
+      name: 'alola',
+      component: AppAlola
+    },
+    {
+      path: '/galar',
+      name: 'galar',
+      component: AppGalar
+    },
+    {
+      path: '/paldea',
+      name: 'paldea',
+      component: AppPaldea
+    },
+    {
+      path: '/hisui',
+      name: 'hisui',
+      component: AppHisui
     },
   ]
 })
