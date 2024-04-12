c9b05f0e82820ccc27fe4848d51961373d6cfd00
alexraduca01 <alex.raduca01@icloud.com>
2024-04-12 14:42:26 +0200

    added router link to home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="bg-dark-red d-flex justify-content-between align-items-center px-3" style="height: 60px;">
-        <img src="/img/logo.png" style="height: 80%;">
+        <router-link :to="'/'">
+            <img src="/img/logo.png" style="height: 50px; filter: drop-shadow(5px 5px 2px rgba(0, 0, 0, 0.5));">
+        </router-link>
+        
         <h1 v-if="store.regionName != ''" class="text-white m-0">{{ store.regionName }}</h1>
     </div>
 </template>
